11ff12689407c9c2edcbd3ab0789a5dd2400e3ad
Trang <bella@axon.com>
2024-12-08 22:34:44 +0700

    chore: delete unused file

---
+++ lession3/file1.js
@@ -0,0 +1,13 @@
+// let name = "Loc";
+// let isLearningPlaywright = true;
+// let price = 100;
+
+// if (price == 110){
+//   console.log("Đúng");
+// }else{
+//     console.log("Sai");
+// }
+
+for(i = 0; i < 10; i++){
+    console.log("gia tri cua i label: " + i);
+}
